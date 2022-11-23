d969fefbeb94cb94310ff4c0f2b0d198e1ab3f41
ab <andre.berlemont@gmail.com>
2022-11-23 15:28:53 +0100

    namespace

--- Runtime/scenes/Editor/WinEdBlueprintScenesProfiler.cs
+++ Runtime/scenes/Editor/WinEdBlueprintScenesProfiler.cs
@@ -3,160 +3,162 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-using fwp.scenes;
-using fwp.halpers;
-
-abstract public class WinEdBlueprintScenesProfiler : EditorWindow
+namespace fwp.scenes
 {
-
-	string[] sections;
-	Dictionary<string, List<SceneProfil>> buttons;
-
-	int tabActive = 0;
-	GUIContent[] tabs;
-	Vector2 tabScroll;
-
-	abstract protected string[] generateSections();
-
-    private void Update()
-    {
-		if(sections == null)
-        {
-			refreshLists(true);
-        }
-    }
-
-    void OnGUI()
+	abstract public class WinEdBlueprintScenesProfiler : EditorWindow
 	{
-		if (GUILayout.Button("refresh refs"))
+
+		string[] sections;
+		Dictionary<string, List<SceneProfil>> buttons;
+
+		int tabActive = 0;
+		GUIContent[] tabs;
+		Vector2 tabScroll;
+
+		abstract protected string[] generateSections();
+
+		private void Update()
 		{
-			refreshLists(true);
-		}
-
-		if (sections == null) return;
-
-		//HalperPrefsEditor.drawToggle("upfold hierarchy", HalperPrefsEditor.ppref_editor_lock_upfold);
-
-		GUILayout.Space(10f);
-
-		tabActive = generateTabsHeader(tabActive, tabs);
-
-		string nm = sections[tabActive];
-		var section = buttons[nm];
-
-		GUILayout.Label($"{nm} has x{section.Count} available scenes");
-
-		tabScroll = GUILayout.BeginScrollView(tabScroll);
-
-		for (int i = 0; i < section.Count; i++)
-		{
-			if (GUILayout.Button(section[i].uid)) // each profil
+			if (sections == null)
 			{
-				//if (EditorPrefs.GetBool(edLoadDebug)) section[i].loadDebug = true;
-				section[i].editorLoad();
-			}
-		}
-		GUILayout.EndScrollView();
-	}
-
-	void refreshLists(bool force = false)
-	{
-		if (buttons == null || force)
-		{
-			buttons = new Dictionary<string, List<SceneProfil>>();
-
-			sections = generateSections();
-            
-			for (int i = 0; i < sections.Length; i++)
-			{
-				var list = getProfils(sections[i]);
-				buttons.Add(sections[i], list);
+				refreshLists(true);
 			}
 		}
 
-
-		if (tabs == null)
+		void OnGUI()
 		{
-			tabs = generateTabsDatas(sections);
-		}
-	}
+			if (GUILayout.Button("refresh refs"))
+			{
+				refreshLists(true);
+			}
 
+			if (sections == null) return;
+			if (buttons == null) return;
 
-	List<SceneProfil> getProfils(string cat)
-	{
-		List<string> names = refreshScenesNamesOfCategory(cat);
+			//HalperPrefsEditor.drawToggle("upfold hierarchy", HalperPrefsEditor.ppref_editor_lock_upfold);
 
-		//Debug.Log("category:" + cat+" has x"+names.Count);
+			GUILayout.Space(10f);
 
-		List<SceneProfil> profils = new List<SceneProfil>();
-		for (int i = 0; i < names.Count; i++)
-		{
-			SceneProfil sp = new SceneProfil(names[i]);
+			tabActive = generateTabsHeader(tabActive, tabs);
 
-			//sp.loadDebug = EditorPrefs.GetBool(edLoadDebug);
+			string nm = sections[tabActive];
+			var section = buttons[nm];
 
-			sp.reload();
+			GUILayout.Label($"{nm} has x{section.Count} available scenes");
 
-			profils.Add(sp);
-		}
-		return profils;
-	}
+			tabScroll = GUILayout.BeginScrollView(tabScroll);
 
-	static public GUIContent[] generateTabsDatas(string[] labels)
-	{
-		GUIContent[] modeLabels = new GUIContent[labels.Length];
-
-		//GUILayout.Label("no mode labels (x" + nms.Length + " candidates)");
-
-		for (int i = 0; i < labels.Length; i++)
-		{
-			modeLabels[i] = new GUIContent(labels[i], "tooltip");
+			for (int i = 0; i < section.Count; i++)
+			{
+				if (GUILayout.Button(section[i].uid)) // each profil
+				{
+					//if (EditorPrefs.GetBool(edLoadDebug)) section[i].loadDebug = true;
+					section[i].editorLoad();
+				}
+			}
+			GUILayout.EndScrollView();
 		}
 
-		return modeLabels;
-	}
-
-	static public int generateTabsHeader(int tabSelected, GUIContent[] tabs)
-	{
-		//GUIStyle gs = new GUIStyle(GUI.skin.button)
-		//int newTab = GUILayout.Toolbar((int)tabSelected, modeLabels, "LargeButton", GUILayout.Width(toolbarWidth), GUILayout.ExpandWidth(true));
-		int newTab = GUILayout.Toolbar((int)tabSelected, tabs, "LargeButton");
-		//if (newTab != (int)tabSelected) Debug.Log("changed tab ? " + tabSelected);
-
-		return newTab;
-	}
-
-	static private List<string> refreshScenesNamesOfCategory(string cat)
-	{
-		List<string> output = new List<string>();
-
-		//string[] scenes = HalperScene.getAllBuildSettingsScenes(false);
-		string[] scenes = HalperScene.getAssetScenesPaths();
-
-		if (scenes.Length <= 0)
+		void refreshLists(bool force = false)
 		{
-			Debug.LogWarning("no scenes ?");
+			if (buttons == null || force)
+			{
+				buttons = new Dictionary<string, List<SceneProfil>>();
+
+				sections = generateSections();
+
+				for (int i = 0; i < sections.Length; i++)
+				{
+					var list = getProfils(sections[i]);
+					buttons.Add(sections[i], list);
+				}
+			}
+
+
+			if (tabs == null)
+			{
+				tabs = generateTabsDatas(sections);
+			}
+		}
+
+
+		List<SceneProfil> getProfils(string cat)
+		{
+			List<string> names = refreshScenesNamesOfCategory(cat);
+
+			//Debug.Log("category:" + cat+" has x"+names.Count);
+
+			List<SceneProfil> profils = new List<SceneProfil>();
+			for (int i = 0; i < names.Count; i++)
+			{
+				SceneProfil sp = new SceneProfil(names[i]);
+
+				//sp.loadDebug = EditorPrefs.GetBool(edLoadDebug);
+
+				sp.reload();
+
+				profils.Add(sp);
+			}
+			return profils;
+		}
+
+		static public GUIContent[] generateTabsDatas(string[] labels)
+		{
+			GUIContent[] modeLabels = new GUIContent[labels.Length];
+
+			//GUILayout.Label("no mode labels (x" + nms.Length + " candidates)");
+
+			for (int i = 0; i < labels.Length; i++)
+			{
+				modeLabels[i] = new GUIContent(labels[i], "tooltip");
+			}
+
+			return modeLabels;
+		}
+
+		static public int generateTabsHeader(int tabSelected, GUIContent[] tabs)
+		{
+			//GUIStyle gs = new GUIStyle(GUI.skin.button)
+			//int newTab = GUILayout.Toolbar((int)tabSelected, modeLabels, "LargeButton", GUILayout.Width(toolbarWidth), GUILayout.ExpandWidth(true));
+			int newTab = GUILayout.Toolbar((int)tabSelected, tabs, "LargeButton");
+			//if (newTab != (int)tabSelected) Debug.Log("changed tab ? " + tabSelected);
+
+			return newTab;
+		}
+
+		static private List<string> refreshScenesNamesOfCategory(string cat)
+		{
+			List<string> output = new List<string>();
+
+			//string[] scenes = HalperScene.getAllBuildSettingsScenes(false);
+			string[] scenes = HalperScene.getAssetScenesPaths();
+
+			if (scenes.Length <= 0)
+			{
+				Debug.LogWarning("no scenes ?");
+				return output;
+			}
+
+			for (int i = 0; i < scenes.Length; i++)
+			{
+				string path = scenes[i].ToLower();
+
+				if (path.Contains("/3rd")) continue;
+
+				if (!path.Contains(cat.ToLower())) continue;
+
+				string scName = scenes[i].Substring(scenes[i].LastIndexOf("/") + 1);
+
+				if (scName.EndsWith(".unity")) scName = scName.Substring(0, scName.IndexOf(".unity"));
+
+				output.Add(scName);
+			}
+
+			//Debug.Log($"found x{regionScenes.Count} regions");
+
 			return output;
 		}
 
-		for (int i = 0; i < scenes.Length; i++)
-		{
-			string path = scenes[i].ToLower();
-
-			if (path.Contains("/3rd")) continue;
-
-			if (!path.Contains(cat.ToLower())) continue;
-
-			string scName = scenes[i].Substring(scenes[i].LastIndexOf("/") + 1);
-
-			if (scName.EndsWith(".unity")) scName = scName.Substring(0, scName.IndexOf(".unity"));
-
-			output.Add(scName);
-		}
-
-		//Debug.Log($"found x{regionScenes.Count} regions");
-
-		return output;
 	}
 
 }
--- Runtime/scenes/SceneTools.cs
+++ Runtime/scenes/SceneTools.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace fpw.scenes
+namespace fwp.scenes
 {
     static public class SceneTools
     {
