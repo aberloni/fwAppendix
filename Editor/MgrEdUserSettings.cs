558cbe0442b10713243e8c9a27677f24685c2a71
abe_laptop <andre.berlemont@gmail.com>
2025-06-16 14:28:02 +0200

    default bool state is OFF (ed user)

--- Editor/MgrEdUserSettings.cs
+++ Editor/MgrEdUserSettings.cs
@@ -10,28 +10,28 @@ namespace fwp.settings.editor
 	{
 		static bool Verbose => Application.isEditor;
 
-		static public bool getBool(string uid) => EditorPrefs.GetFloat(uid, 1f) > 0f;
+		static public bool getBool(string uid) => EditorPrefs.GetFloat(uid, 0f) > 0f;
 		static public void setBool(string uid, bool val)
 		{
 			EditorPrefs.SetFloat(uid, val ? 1f : 0f);
 			log(uid, val);
 		}
 
-		static public float getFloat(string uid, float def) => EditorPrefs.GetFloat(uid, def);
+		static public float getFloat(string uid, float def = 0f) => EditorPrefs.GetFloat(uid, def);
 		static public void setFloat(string uid, float val)
 		{
 			EditorPrefs.SetFloat(uid, val);
 			log(uid, val);
 		}
 
-		static public int getInt(string uid, int def) => EditorPrefs.GetInt(uid, def);
+		static public int getInt(string uid, int def = 0) => EditorPrefs.GetInt(uid, def);
 		static public void setInt(string uid, int val)
 		{
 			EditorPrefs.SetInt(uid, val);
 			log(uid, val);
 		}
 
-		static public string getString(string uid, string def) => EditorPrefs.GetString(uid, def);
+		static public string getString(string uid, string def = "") => EditorPrefs.GetString(uid, def);
 		static public void setString(string uid, string val)
 		{
 			EditorPrefs.SetString(uid, val);
