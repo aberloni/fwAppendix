84cc02d16e27e637cb6181e7c92e6980038ccbae
ab <andre.berlemont@gmail.com>
2025-10-01 13:31:11 +0200

    toggle verbose

--- Editor/MgrEdUserSettings.cs
+++ Editor/MgrEdUserSettings.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 namespace fwp.settings.editor
 {
 	using UnityEditor;
-	
+
+	/// <summary>
+	/// https://discussions.unity.com/t/editor-how-to-add-checkmarks-to-menuitems/114525
+	/// https://docs.unity3d.com/ScriptReference/Menu.SetChecked.html
+	/// </summary>
 	static public class MgrEdUserSettings
 	{
-		static bool Verbose => Application.isEditor;
+		const string ed_user_settings = "Tools/Appendix/User settings verbose";
+
+		[MenuItem(ed_user_settings)]
+		static public void toggleUserSettingsVerbose()
+		{
+			Menu.SetChecked(ed_user_settings, !Verbose);
+			Debug.Log("user.settings.verbose : " + Verbose);
+		}
+
+		static bool Verbose => Menu.GetChecked(ed_user_settings);
 
 		static public bool getBool(string uid) => EditorPrefs.GetFloat(uid, 0f) > 0f;
 		static public void setBool(string uid, bool val)
